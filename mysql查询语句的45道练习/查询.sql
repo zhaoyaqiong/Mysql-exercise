bf622013ca797d6d9d9d9a05b12ddfc218e22e2f
赵亚琼 <waishushu@outlook.com>
2017-09-23 10:47:25 +0800

    添加几个题目和答案

--- mysql查询语句的45道练习/查询.sql
+++ mysql查询语句的45道练习/查询.sql
@@ -32,3 +32,51 @@ select Sno from Score where Degree between 70 and 90;
 -- 15.查询所有学生的Sname、Cno和Degree列。
 select st.Sname,sc.Cno,sc.Degree from Student st,Score sc where st.Sno = sc.Sno;
 select st.Sname,sc.Cno,sc.Degree from Student st inner join Score sc on st.Sno = sc.Sno;
+-- 16.查询所有学生的Sname、Cname和Degree列。
+select Sname,Cname,Degree from Student inner join Score on Student.Sno = Score.sno inner join Course on Course.Cno = score.Cno;
+-- 17.查询“95033”班学生的平均分。(这里95033没有成绩 = =)
+select class,avg(Degree) from Student,Score where Student.Sno = Score.Sno group by Student.Class having Student.Class = '95033';
+-- 18.假设使用如下命令建立了一个grade表：
+create table grade(low  int(3),upp  int(3),rank  char(1));
+insert into grade values(90,100,'A');
+insert into grade values(80,89,'B');
+insert into grade values(70,79,'C');
+insert into grade values(60,69,'D');
+insert into grade values(0,59,'E');
+-- 现查询所有同学的Sno、Cno和rank列。
+select st.Sno,sc.Cno,gr.rank from Student st,Score sc,grade gr where st.Sno = sc.Sno and sc.Degree between gr.low and gr.upp;
+select Sno,Cno,rank from Score,grade where degree between low and upp;
+-- 19.查询选修“3-105”课程的成绩高于“109”号同学成绩的所有同学的记录。
+select * from Score where Cno = '3-105' and Degree > all(select Degree from Score where Sno = '109');
+-- 20.选了多门课程并且是这个课程下不是最高分的
+select * from Score where Sno in (select Sno from Score group by Sno having count(*)>=2);
+-- 21.查询成绩高于学号为“109”、课程号为“3-105”的成绩的所有记录。
+select * from score where cno = '3-105' and degree > (select degree from score where sno='109' and cno='3-105');
+-- 22.查询和学号为108、101的同学同年出生的所有学生的Sno、Sname和Sbirthday列。
+select Sno,Sname from Student where year(Sbirthday) in (select year(Sbirthday) from Student where Sno='108' or  Sno = '101') and Sno != '108' and Sno != '101';
+-- 23.查询“张旭“教师任课的学生成绩。
+select st.Sname,sc.Degree from Student st,Score sc where st.Sno = sc.Sno and sc.Cno in (select Cno from Course c,Teacher t where c.Tno = t.Tno and t.Tname = '张旭');
+-- 24.查询选修某课程的同学人数多于5人的教师姓名。(注意一下这里也没有符合条件的，可以自己再添加一些数据)
+select t.Tname from Teacher t,Course c where t.Tno = c.Tno and c.Cno in (select Cno from Score group by Cno having count(*)>5);
+-- 25.查询95033班和95031班全体学生的记录。
+select * from Student where Class in('95033','95031');
+-- 26.查询存在有85分以上成绩的课程Cno。
+select distinct Cno from Score where Degree > 85;
+-- 27.查询出“计算机系“教师所教课程的成绩表。
+select * from Score s,Course c where s.Cno = C.Cno and C.Tno in(select Tno from Teacher where Depart='计算机系');
+-- 28.查询“计算 机系”与“电子工程系“不同职称的教师的Tname和Prof。
+select Tname,Prof from Teacher where Depart in('计算机系','电子工程系');
+-- 29.查询选修编号为“3-105“课程且成绩至少高于选修编号为“3-245”的同学的Cno、Sno和Degree,并按Degree从高到低次序排序。
+select Cno,Sno,Degree from Score where Cno = '3-105' and Degree > any(select Degree from Score where Cno='3-235');
+-- 30.查询选修编号为“3-105”且成绩高于选修编号为“3-245”课程的同学的Cno、Sno和Degree.
+select Cno,Sno,Degree from Score where Cno = '3-105' and Degree > all(select Degree from Score where Cno='3-235');
+
+
+
+
+
+
+
+
+
+
